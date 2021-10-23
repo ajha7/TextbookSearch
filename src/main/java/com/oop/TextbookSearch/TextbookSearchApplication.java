84fc4ef481293fe49688200fd5013a3bd793a379
Anshul Jha <65700919+ajha7@users.noreply.github.com>
2021-10-23 01:59:07 -0700

    Set up Spring Boot project, added unfinished base Controller class

---
+++ src/main/java/com/oop/TextbookSearch/TextbookSearchApplication.java
@@ -0,0 +1,13 @@
+package com.oop.TextbookSearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TextbookSearchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TextbookSearchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/oop/TextbookSearch/TextbookSearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oop.TextbookSearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TextbookSearchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
