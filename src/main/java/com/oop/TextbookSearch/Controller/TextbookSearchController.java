29627b667ac5a9ccac5c5860dff9f307d5dfb217
ekamgrewal <ekamjeet.grewal315@gmail.com>
2021-10-26 22:35:08 -0700

    Initial controller commiy" git commit

--- src/main/java/com/oop/TextbookSearch/Controller/TextbookSearchController.java
+++ src/main/java/com/oop/TextbookSearch/Controller/TextbookSearchController.java
@@ -5,4 +5,20 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 public class TextbookSearchController {
 
+    private Textbook textbook;
+
+    public Controller(Textbook textbook) {
+		this.textbook = textbook;
+	}
+
+    public void invokeTextbookSearch(String title, String ISBN, String author, int yearPublished, String language){
+
+
+
+    }
+
+    public void updateView(){
+        
+    }
+
 }
