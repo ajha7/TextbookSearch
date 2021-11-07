4cb6e9809b23700873da52b553ed891559062843
Anshul Jha <65700919+ajha7@users.noreply.github.com>
2021-11-07 13:51:14 -0800

    set up automatic routing to index.html, changed controller to route to index.html

--- src/main/java/com/oop/TextbookSearch/Controller/TextbookSearchController.java
+++ src/main/java/com/oop/TextbookSearch/Controller/TextbookSearchController.java
@@ -1,20 +1,31 @@
 package com.oop.TextbookSearch.Controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+import com.oop.TextbookSearch.Model.Textbook;
 
-@RestController
+@Controller
 public class TextbookSearchController {
 
     private Textbook textbook;
 
-    public Controller(Textbook textbook) {
+    public TextbookSearchController() {
+    	
+    }
+    
+    public TextbookSearchController(Textbook textbook) {
 		this.textbook = textbook;
 	}
 
+    
+    @RequestMapping(value="/")
+    public String homePage() {
+    	return "index";
+    }
+    
     public void invokeTextbookSearch(String title, String ISBN, String author, int yearPublished, String language){
 
-
-
+    	
     }
 
     public void updateView(){
