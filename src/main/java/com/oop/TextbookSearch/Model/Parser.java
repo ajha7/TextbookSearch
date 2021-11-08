6eb7028b47cfba84a8ae9eef2f7dce87b6159adc
ekamgrewal <ekamjeet.grewal315@gmaiol.com>
2021-11-07 23:57:07 -0800

    first parser commit

--- src/main/java/com/oop/TextbookSearch/Model/Parser.java
+++ src/main/java/com/oop/TextbookSearch/Model/Parser.java
@@ -1,4 +1,12 @@
 package com.oop.TextbookSearch.Model;
+import java.io.IOException;
+
+import javax.lang.model.element.Element;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Elements;
+import org.jsoup.select.Document;
 
 public class Parser {
 
@@ -11,5 +19,27 @@ public class Parser {
 		Textbook zlibTextbook = new Textbook();
 		return zlibTextbook;
 	}
+
+	public static void main(String[]args){
+        Document d = null;
+        try{
+            d = Jsoup.connect("http://libgen.li/").get();
+            String title = d.title();
+            System.out.println("Heading : " + title);
+
+            Elements link = d.select("a[href]");
+
+            for(Elements link : links){
+                System.out.println("\nLink : "+ link.attr("href"));
+                System.out.println("Heading : " + link.text());
+
+            }
+
+
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+
+
 
 }
